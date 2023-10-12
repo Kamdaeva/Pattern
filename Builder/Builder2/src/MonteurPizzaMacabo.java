ca1e7722be545ccfd5fafd70372a943b0e953f25
kamdaeva <malvina.kamd@facsciences-uy1.cm>
2023-10-12 10:54:17 +0100

    builder

---
+++ Builder/Builder2/src/MonteurPizzaMacabo.java
@@ -0,0 +1,8 @@
+public class MonteurPizzaMacabo extends MonteurPizza{
+    public void monterPate()
+    { pizza.setPate("feuilletée de macabo"); }
+    public void monterSauce()
+    { pizza.setSauce("sauce aux deux epices"); }
+    public void monterGarniture() { pizza.setGarniture("escargot");
+    }
+}
