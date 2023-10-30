514a3039456a7c98f1a116111445da508d060706
kamdaeva <malvina.kamd@facsciences-uy1.cm>
2023-10-30 20:49:59 +0100

    patron adapter exemple1

---
+++ code/Structuration/Example1/src/DocumentHtml.java
@@ -0,0 +1,10 @@
+public class DocumentHtml implements Documents{
+    public DocumentHtml(){}
+
+    public int setContenu(int contenu){
+        return contenu;
+    }
+    public int dessine(){
+        
+    }
+}
