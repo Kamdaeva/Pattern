07e2fdee99f60148d5667dc1143051df268deaf6
kamdaeva <malvina.kamd@facsciences-uy1.cm>
2023-10-31 05:25:39 +0100

    exemple2 Methode1

--- code/Structuration/Example1/src/ServeurWeb.java
+++ code/Structuration/Example1/src/ServeurWeb.java
@@ -2,9 +2,10 @@ public class ServeurWeb {
     public static void main(String[] args){
         ComposantPdf outilPdf = new ComposantPdf();
         Documents doc = new DocumentPdf(outilPdf);
+        
+        System.out.println("document : " + doc.dessine());
+        System.out.println("document : " + doc.setContenu(5));
+        System.out.println("document : " + doc.imprime());
 
-        doc.dessine();
-        doc.setContenu(1);
-        doc.imprime();
     }
 }
