8658e95f5691f9a109ad278d6f2cbe7dc045c7b1
kamdaeva <malvina.kamd@facsciences-uy1.cm>
2023-11-14 08:58:23 +0100

    bridge1

---
+++ code/Structuration/Composite/Cours/src/Paragraphe.java
@@ -0,0 +1,8 @@
+public class Paragraphe extends Texte{
+    public Paragraphe(String titre, String preambule){
+        super(titre, preambule);
+    }
+    public void ajout(Texte txt){
+        // return System.out.println("paragraphe " + titre + " de preambule " + preambule);
+    }
+}
